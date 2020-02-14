928b2e7fa66c6c73ed81a7fdc58730ac430ce3c7
Aditya <47950273+Adityaverma1998@users.noreply.github.com>
2020-02-14 15:26:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app  = express()
 const path = require('path')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-// app.use(passport.initialize());
-// app.use(passport.session());
+ app.use(passport.initialize());
+ app.use(passport.session());
 
 
 app.use('/',express.static(path.join(__dirname,'public')))
